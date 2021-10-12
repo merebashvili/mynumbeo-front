373401edaf64d85afd290f221c7a889ec9e208fe
Lasha Merebashvili <merebashvili.work@gmail.com>
2021-10-12 20:24:54 +0400

    Display countries with async pipe

--- src/app/countries-list/countries-list.component.ts
+++ src/app/countries-list/countries-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CountryService } from '../country.service';
 import { Country } from '../country';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-countries-list',
@@ -8,7 +9,11 @@ import { Country } from '../country';
   styleUrls: ['./countries-list.component.scss'],
 })
 export class CountriesListComponent implements OnInit {
-  public countries: Country[] = [];
+  // ! definite assignment assertion tells to the compiler:
+  // "There is a property called countries$ with a type of Observable<Country[]> | undefined.
+  // It starts with a value of undefined. But every time I get or set
+  // that property, I want to treat it as Observable<Country[]>."
+  public countries$!: Observable<Country[]>;
 
   constructor(private countryService: CountryService) {}
 
@@ -17,8 +22,6 @@ export class CountriesListComponent implements OnInit {
   }
 
   getCountries(): void {
-    this.countryService
-      .getCountries()
-      .subscribe((countries) => console.log(countries));
+    this.countries$ = this.countryService.getCountries();
   }
 }
