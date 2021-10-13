3a08d2dd5d67bf7fc30b0e3d59d474ef17c38a91
Lasha Merebashvili <merebashvili.work@gmail.com>
2021-10-13 15:45:10 +0400

    Create country component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CountriesListComponent } from './countries-list/countries-list.component';
+import { CountryComponent } from './country/country.component';
 
 @NgModule({
-  declarations: [AppComponent, CountriesListComponent],
+  declarations: [AppComponent, CountriesListComponent, CountryComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
