3e67f07ed7bfa67cbbf1924eb3aad8b01c7be5f4
Lasha Merebashvili <merebashvili.work@gmail.com>
2021-10-13 15:53:04 +0400

    Set up routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CountriesListComponent } from './countries-list/countries-list.component';
+import { CountryComponent } from './country/country.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'countries-list', component: CountriesListComponent },
+  { path: 'country/:id', component: CountryComponent },
+  { path: '', redirectTo: '/countries-list', pathMatch: 'full' },
+  { path: '**', redirectTo: '/countries-list', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
