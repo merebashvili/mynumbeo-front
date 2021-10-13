bf06bf7f8945a4ad6882115d69217fa18c7a545c
Lasha Merebashvili <merebashvili.work@gmail.com>
2021-10-13 15:31:24 +0400

    Transform country data to the desired form

--- src/app/country.service.ts
+++ src/app/country.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Country } from './country';
+import { Country, ResponseCountry } from './country';
 import { HttpClient } from '@angular/common/http';
+import { tap, map } from 'rxjs/operators';
+import { Product } from './product';
 
 @Injectable({
   providedIn: 'root',
@@ -12,6 +14,44 @@ export class CountryService {
   constructor(private http: HttpClient) {}
 
   getCountries(): Observable<Country[]> {
-    return this.http.get<Country[]>(this.countriesUrl);
+    return this.http.get<ResponseCountry[]>(this.countriesUrl).pipe(
+      // maybe I should do this transformation to the back end and
+      // deliver ready data
+
+      map((countries: ResponseCountry[]) =>
+        countries.map(
+          (country: ResponseCountry): Country =>
+            this.transformResponseCountry(country)
+        )
+      )
+    );
+  }
+
+  calculateTotalExpenses(products: Product[]): number {
+    const initialValue = 0;
+
+    const totalExpenseInGlobalCurrency: number = products.reduce(
+      (previousValue, currentValue) => {
+        return (
+          previousValue +
+          currentValue.price_in_usd * currentValue.quantity_for_month
+        );
+      },
+      initialValue
+    );
+
+    return totalExpenseInGlobalCurrency;
+  }
+
+  transformResponseCountry(responseCountry: ResponseCountry): Country {
+    const transformedCountryData: Country = {
+      _id: responseCountry._id,
+      name: responseCountry.name,
+      quantity_of_products: responseCountry.products.length,
+      totalExpenseInGlobalCurrency: this.calculateTotalExpenses(
+        responseCountry.products
+      ),
+    };
+    return transformedCountryData;
   }
 }
--- src/app/country.ts
+++ src/app/country.ts
@@ -1,10 +1,17 @@
 import { Product } from './product';
 
 // Interface is used solely for type-checking purpose
-export interface Country {
+export interface ResponseCountry {
   _id: string;
   name: string;
   products: Array<Product>;
   owner: string;
   __v: number;
+}
+
+export interface Country {
+  _id: string;
+  name: string;
+  quantity_of_products: number;
+  totalExpenseInGlobalCurrency: number;
 }
