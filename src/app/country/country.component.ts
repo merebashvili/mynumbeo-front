bca4e9ce21e17e11850cf40656a52c99c01d29f0
Lasha Merebashvili <merebashvili.work@gmail.com>
2021-10-14 01:28:58 +0400

    Add routerlink and change service address

--- src/app/country/country.component.ts
+++ src/app/country/country.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { CountryService } from '../country.service';
+import { CountryService } from '../services/country.service';
 import { ResponseCountry } from '../country';
 
 @Component({
