eed4f5f479b8ed08660b1d208e3e23615de2c3d8
Lasha Merebashvili <merebashvili.work@gmail.com>
2021-10-19 16:40:57 +0400

    Create auth form with template-driven approach

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.scss']
+  styleUrls: ['./auth.component.scss'],
 })
 export class AuthComponent implements OnInit {
+  public isLoginMode = true;
+  //Must contain at least one English letter (uppercase or lowercase) and at least one digit
+  public passwordRegex = '^(?=.*?[a-zA-Z])(?=.*?[0-9]).*$';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  public onSwitchMode(): void {
+    this.isLoginMode = !this.isLoginMode;
   }
 
+  public onSubmit(form: NgForm): void {
+    form.reset();
+  }
 }
