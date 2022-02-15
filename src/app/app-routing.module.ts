45c147e304afafd41cf4c5bac09103e1050c1a0d
Lasha Merebashvili <merebashvili.work@gmail.com>
2022-02-15 11:12:50 +0400

    Add auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,14 @@ import { CountriesListComponent } from './countries-list/countries-list.componen
 import { CountryComponent } from './country/country.component';
 import { ProductComponent } from './product/product.component';
 import { AuthComponent } from './auth/auth.component';
+import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
-  { path: 'country-list', component: CountriesListComponent },
+  {
+    path: 'country-list',
+    component: CountriesListComponent,
+    canActivate: [AuthGuard],
+  },
   { path: 'country/:id', component: CountryComponent },
   { path: 'product/:id', component: ProductComponent },
   { path: 'product', component: ProductComponent },
---
+++ src/app/auth/auth.guard.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+import { map, take, tap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    return this.authService.isAuthenticated$.pipe(
+      take(1),
+      map((isAuth) => (isAuth ? true : this.router.createUrlTree(['/auth'])))
+    );
+  }
+}
